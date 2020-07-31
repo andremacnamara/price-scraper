0b11a6208541987beff4ea0c26f405b75dd3849a
Andre MacNamara <andremacnamara12@gmail.com>
2020-07-31 22:14:50 +0100

    .

--- index.mjs
+++ index.mjs
@@ -2,7 +2,7 @@ import express from 'express';
 
 import { listOfItems, returnItemData, getData } from './lib/scraper.mjs'
 import { wait } from './lib/utils.mjs'
-//import './lib/cron.mjs'
+import './lib/cron.mjs'
 
 const app = express();
 
@@ -12,18 +12,13 @@ app.get('/scrape', async (req, res, next) => {
 
 app.get('/', async ( req, res, next ) => {
     const [items] = await Promise.all([getData()]);
-    // items.forEach(item => {
+    // items.forEach(item => {/
     //     item.date = new Date(item.date);
     // });
 
     res.json({items})
 })
 
-app.get('/items', async ( req, res, next ) => {
-    const [items] = await Promise.all([returnItemData('https://www.ralphlauren.ie/en/cotton-quarter-zip-sweater-3615731966197.html', '.product-price span', 'h1.product-name', '#s7viewer-main .popup-img.zoomable')]);
-    console.log(items)
-    res.json({'status': 'ff'})
-})
 
 const server = app.listen(2300, () => {
     console.log(`Scraper is running on port: ${server.address().port}`);
